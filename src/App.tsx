e6b2f58aea9c20f2626a5b842d5b970abd6ea8c1
MigueldCast <miguel.acastillodiaz@outlook.es>
2023-02-17 23:13:31 -0500

    First commit: react+typescript project configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>Personal Portfolio with React and Typescript</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
